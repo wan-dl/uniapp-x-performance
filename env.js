4f2c3fdc924ab9150d5160e34d6e04b41d149f78
wan201809 <53882753+wan201809@users.noreply.github.com>
2025-04-18 16:59:56 +0800

    新增 uni_modules/app-performance 鸿蒙支持获取CPU使用率

---
+++ env.js
@@ -0,0 +1,34 @@
+// 参考文档: https://uniapp.dcloud.net.cn/worktile/auto/hbuilderx-extension/#envjs
+// 备注：UNI_TEST_CUSTOM_ENV 用于自动化测试向uniapp-cli-vite编译器中传递一些环境变量，设置格式：key: value
+module.exports = {
+    "is-custom-runtime": false,
+    "UNI_TEST_CUSTOM_ENV": {},
+    "compile": true,
+    "h5": {
+        "options": {
+            "headless": true
+        },
+        "executablePath": ""
+    },
+    "mp-weixin": {
+        "port": 9420,
+        "account": "",
+        "args": "",
+        "cwd": "",
+        "launch": true,
+        "teardown": "disconnect",
+        "remote": false,
+        "executablePath": ""
+    },
+    "app-plus": {
+        "android": {
+            "id": "",
+            "executablePath": ""
+        },
+        "version": "",
+        "ios": {
+            "id": "",
+            "executablePath": ""
+        }
+    }
+}
